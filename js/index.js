243c90a7e5d9c5248c6cc966f7c61d8b84247bb9
Raymondg3131 <114846526+Raymondg3131@users.noreply.github.com>
2025-09-12 17:16:51 -0400

    changes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let one=document.getElementById("one");
+let two=document.getElementById("two");
+let output = document.getElementById("output");
+
+function doAdd(){
+    output.innerHTML=one.value+two.value;
+}
